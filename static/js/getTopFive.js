e47deaec6b5c12eb5a1250830c9f947c4409b7f8
eddcode123 <edwinmutugi29@gmail.com>
2024-07-03 14:34:23 +0300

    updating api security

--- static/js/getTopFive.js
+++
@@ -1,9 +0,0 @@
-fetch('http://localhost:3000/api/top-artists')
-    .then(response => response.json())
-    .then(data => {
-        // Process the fetched data
-        console.log(data); // Example: Log data to console
-        // Update UI or perform actions with fetched data
-        // Example: Update DOM elements with fetched data
-    })
-    .catch(error => console.error('Error fetching data:', error));
