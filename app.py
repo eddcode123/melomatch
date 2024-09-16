ac5a61570f77c29e93d298c790d07993f40b4d20
eddcode123 <edwinmutugi29@gmail.com>
2024-09-16 14:44:24 +0300

    updating project frontend

--- app.py
+++ app.py
@@ -7,6 +7,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template("aboutus.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/features')
+def features():
+    return render_template("features.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
